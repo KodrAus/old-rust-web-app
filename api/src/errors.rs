bac1ad183fe4cda489346b19703eb47b192dd33e
Ashley Mannix <ashleymannix@live.com.au>
2016-11-20 17:04:20 +1000

    fill in data model

--- api/src/errors.rs
+++ api/src/errors.rs
@@ -1,8 +1,19 @@
+use redis;
+use serde_json;
+
 error_chain! {
+    foreign_links {
+        redis::RedisError, RedisError;
+        serde_json::Error, JsonError;
+    }
 	errors {
         NotAnId {
             description("the given value isn't a valid id")
             display("the given value isn't a valid id")
+        }
+        PersonNotFound {
+            description("the requested person doesn't exist")
+            display("the requested person doesn't exist")
         }
     }
 }
@@ -13,6 +24,7 @@ use iron::status::Status;
 impl From<Error> for IronError {
     fn from(err: Error) -> IronError {
         match err {
+            e @ Error(ErrorKind::PersonNotFound, _) => IronError::new(e, Status::NotFound),
             e => IronError::new(e, Status::InternalServerError),
         }
     }
--- api/src/main.rs
+++ api/src/main.rs
@@ -9,9 +9,10 @@ extern crate json_str;
 extern crate iron;
 extern crate router;
 
+extern crate redis;
+
 #[macro_use]
 extern crate serde_derive;
-
 extern crate serde;
 extern crate serde_json;
 
@@ -22,10 +23,12 @@ pub mod errors;
 pub mod model;
 
 use std::convert::TryInto;
+use redis::Commands;
 use iron::prelude::*;
 use iron::status;
 use router::Router;
 
+use errors::*;
 use model::*;
 
 macro_rules! get_id {
@@ -38,6 +41,11 @@ macro_rules! get_id {
     )
 }
 
+fn get_conn() -> Result<redis::Connection> {
+    let client = redis::Client::open("redis://127.0.0.1/")?;
+    client.get_connection().map_err(|e| e.into())
+}
+
 fn main() {
     let mut router = Router::new();
     router.get("/person/:id", get_person, "get_person");
@@ -48,12 +56,47 @@ fn main() {
     fn get_person(req: &mut Request) -> IronResult<Response> {
         let id: Id = get_id!(req);
 
-        Ok(Response::with((status::Ok, id.as_ref())))
+        let conn = get_conn()?;
+
+        // Maybe get the serialised person from Redis
+        let person_data: Option<String> = conn
+            .get(id.as_ref())
+            .map_err(|e| Error::from(e))?;
+
+        // Unwrap the serialised data, or return a `PersonNotFound` error
+        let person_data = person_data.ok_or(Error::from(ErrorKind::PersonNotFound))?;
+
+        // Deserialise the person
+        let person: Person = serde_json::from_str(&person_data)
+            .map_err(|e| Error::from(e))?;
+
+        Ok(Response::with((status::Ok, person_data)))
     }
 
     fn post_person(req: &mut Request) -> IronResult<Response> {
+        #[derive(Deserialize)]
+        struct PostPersonCommand {
+            pub name: String
+        }
+
         let id: Id = get_id!(req);
 
-        Ok(Response::with((status::Ok, id.as_ref())))
+        let conn = get_conn()?;
+
+        let person: PostPersonCommand = serde_json::from_reader(&mut req.body)
+            .map_err(|e| Error::from(e))?;
+
+        let person = Person {
+            id: id,
+            name: person.name
+        };
+
+        let person_data = serde_json::to_string(&person)
+            .map_err(|e| Error::from(e))?;
+
+        conn.set(person.id.as_ref(), person_data)
+            .map_err(|e| Error::from(e))?;
+
+        Ok(Response::with(status::Ok))
     }
 }
